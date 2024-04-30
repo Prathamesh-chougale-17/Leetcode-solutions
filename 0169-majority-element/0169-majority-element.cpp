45e0ab11f7e4491a64ea35743669b406b14d8128
PRATHAMESH CHOUGALE <106848418+Prathamesh-chougale-17@users.noreply.github.com>
2024-04-30 12:06:11 +0530

    Time: 16 ms (68.5%), Space: 27.7 MB (23.64%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -5,7 +5,9 @@ public:
         for(auto &i:nums){
             m[i]++;
         }
-        auto max = max_element(m.begin(),m.end(),[](pair<int,int> a,pair<int,int> b){return a.second<b.second;});
-        return max->first;
+        for(auto &i:m){
+            if(i.second>nums.size()/2) return i.first;
+        }
+        return 0;
     }
 };
