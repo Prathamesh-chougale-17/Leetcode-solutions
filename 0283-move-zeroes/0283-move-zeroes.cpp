3346d46b039f5aecc9fd690f367fe0ef78abb69c
PRATHAMESH CHOUGALE <106848418+Prathamesh-chougale-17@users.noreply.github.com>
2024-04-30 09:52:19 +0530

    Time: 24 ms (13.79%), Space: 21.6 MB (73.33%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size(),count{0};
+        for(auto it=nums.begin();it!=nums.end();it++){
+            if(*it==0) {
+                nums.erase(it);
+                it--;
+                count++;
+            }
+        }
+        for(int i=0;i<count;i++){
+            nums.push_back(0);
+        }
+    }
+};
