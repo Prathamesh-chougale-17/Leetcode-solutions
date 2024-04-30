79f7c46ab121912f1bcb159ef8358add7ef54079
PRATHAMESH CHOUGALE <106848418+Prathamesh-chougale-17@users.noreply.github.com>
2024-04-30 11:45:48 +0530

    Time: 23 ms (21.7%), Space: 27.9 MB (12.67%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,23 +1,11 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int max{0},n{0};
-        long long z{0};
-        if(nums.size()==1) return nums[0];
-        for(int i=1;i<nums.size();i++){
-            if(nums[i-1]==nums[i]) n++;
-            else {
-                if(max<n){
-                    max=n;
-                    z = nums[i-1];
-                }
-                n=1;
-            }
+        unordered_map<int,int> m;
+        for(auto &i:nums){
+            m[i]++;
         }
-        if(max<n){
-                    z = nums[nums.size()-1];
-                }
-                return z;
+        auto max = max_element(m.begin(),m.end(),[](pair<int,int> a,pair<int,int> b){return a.second<b.second;});
+        return max->first;
     }
 };
