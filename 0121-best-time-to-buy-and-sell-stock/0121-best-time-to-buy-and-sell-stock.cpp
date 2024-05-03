1bc97c411732ddc157f10c9c6875172232e0b779
PRATHAMESH CHOUGALE <106848418+Prathamesh-chougale-17@users.noreply.github.com>
2024-05-03 12:32:40 +0530

    Time: 90 ms (41.52%), Space: 95.7 MB (68.82%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -8,20 +8,21 @@ public:
         // auto min = min_element(prices.begin(),prices.end());
         // auto max = max_element(min,prices.end());
         // return *max-*min;
-        int lsf = INT_MAX;
-        int op = 0;
-        int pist = 0;
-        
-        for(int i = 0; i < prices.size(); i++){
-            if(prices[i] < lsf){
-                lsf = prices[i];
-            }
-            pist = prices[i] - lsf;
-            if(op < pist){
-                op = pist;
-            }
+        // int diff{0};
+        // for(int i=0;i<prices.size();i++){
+        //     auto mini = min_element(prices.begin(),prices.end()-i);
+        //     // auto maxi = max_element(mini,prices.end()-i);
+        //     diff = max((prices[prices.size()-1-i]-*mini),diff);
+        // }
+        // return diff;
+
+        int mini{prices[0]},profit{0},cost{0};
+        for(int i=1;i<prices.size();i++){
+            cost = prices[i]-mini;
+            if(cost>profit) profit = cost;
+            mini = min(mini,prices[i]);
         }
-        return op;
+        return profit;
         
     }
 };
