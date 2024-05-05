3c93dcbfeabcf02e792948afedd023c64a8f0ef3
PRATHAMESH CHOUGALE <106848418+Prathamesh-chougale-17@users.noreply.github.com>
2024-05-05 16:54:35 +0530

    Time: 6 ms (18.23%), Space: 14.8 MB (15.28%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        int n = nums.size(),isdec{1},breakpoint{0};
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                breakpoint=i;
+                isdec=0;
+                break;
+            }
+        }
+        if(!isdec){
+            int jmax{INT_MAX},maxi{0},mini=nums[breakpoint],swapp{0};
+            for(int i=breakpoint+1;i<n;i++){
+                maxi = max(mini,nums[i]);
+                // jmax = min(maxi,jmax);
+                if(jmax>maxi && maxi!=mini){
+                    jmax = maxi;
+                    swapp = i;
+                    cout<<i<<maxi<<endl;
+                }
+            }
+            swap(nums[breakpoint],nums[swapp]);
+            // cout<<*(nums.begin());
+            sort(nums.begin()+breakpoint+1,nums.end());
+        }
+        else{
+            sort(nums.begin(),nums.end());
+        }
     }
 };
