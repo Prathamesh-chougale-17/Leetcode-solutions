dd24e8e28163995818682e68412cfb57702d2b43
PRATHAMESH CHOUGALE <106848418+Prathamesh-chougale-17@users.noreply.github.com>
2024-07-07 07:55:21 +0530

    Time: 2 ms (53.84%), Space: 7.1 MB (72.3%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.cpp
+++ 1642-water-bottles/1642-water-bottles.cpp
@@ -1,21 +1,7 @@
 class Solution {
 public:
-    int numWaterBottles(int numBottles, int numExchange) {
-        
-       int ans = numBottles, count = 0, curBottles = 0;
-        while(true)
-        {
-            if(numBottles - numExchange < 0)
-            {
-                numBottles += curBottles;
-                ans += count;
-                count = curBottles = 0;
-            }
-            if(numBottles - numExchange < 0) break;
-            count++, curBottles++;
-            numBottles -= numExchange;
-        }
-        return ans;
-        
+    int numWaterBottles(int n, int k) {
+        if (n < k) return n;
+        return k + numWaterBottles(n - k + 1, k);
     }
 };
