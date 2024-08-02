661236fa5ae874e9244c61087e0218ffa627af3b
PRATHAMESH CHOUGALE <106848418+Prathamesh-chougale-17@users.noreply.github.com>
2024-08-02 10:34:24 +0530

    Time: 5 ms (22.17%), Space: 9.3 MB (6.85%) - LeetHub

--- 2571-find-the-pivot-integer/2571-find-the-pivot-integer.cpp
+++ 2571-find-the-pivot-integer/2571-find-the-pivot-integer.cpp
@@ -2,24 +2,15 @@ class Solution {
 public:
     int pivotInteger(int n) {
         
-        // vector<int> arr(n);
-        // arr[0]=1;
-        // for(int i=1;i<n;i++){
-        //     arr[i] = arr[i-1] + (i+1);
-        // }
-        // for(int i = 1;i<n;i++){
-        //     if(arr[i]==(arr[n-1]-arr[i-1])) return i+1;
-        //     if(arr[i]>(arr[n-1]-arr[i-1])) return -1;
-        // }
-        // return 1;
-         int x = n*n + n;
-        cout << x << endl;
-        x /= 2;
-        int s = sqrt(x);
-        cout << s << endl;
-        if ((s*s) == x) {
-            return s;
+        vector<int> arr(n);
+        arr[0]=1;
+        for(int i=1;i<n;i++){
+            arr[i] = arr[i-1] + (i+1);
         }
-        return -1;
+        for(int i = 1;i<n;i++){
+            if(arr[i]==(arr[n-1]-arr[i-1])) return i+1;
+            if(arr[i]>(arr[n-1]-arr[i-1])) return -1;
+        }
+        return 1;
     }
 };
