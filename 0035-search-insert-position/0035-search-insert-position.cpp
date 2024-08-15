ab8bf189faee7a1f0f9943be8c8d7e14a85b0d84
PRATHAMESH CHOUGALE <106848418+Prathamesh-chougale-17@users.noreply.github.com>
2024-08-15 14:37:45 +0530

    Time: 0 ms (100%), Space: 12.4 MB (36.69%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        return lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+        int r=nums.size()-1,l=0,ans=nums.size(),mid=0;
+        while(r>=l){
+            mid=l+(r-l)/2;
+            if(nums[mid]>=target){
+                ans = mid;
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return ans;
     }
 };
