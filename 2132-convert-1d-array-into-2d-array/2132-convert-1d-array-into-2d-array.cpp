de2da1a94c22f5074f90310e5c2bda21c8a23a56
PRATHAMESH CHOUGALE <106848418+Prathamesh-chougale-17@users.noreply.github.com>
2024-09-01 08:05:00 +0530

    Time: 68 ms (94.05%), Space: 88 MB (73.42%) - LeetHub

---
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isvalid(vector<int>& original, int m, int n){
+        return original.size()==m*n?true:false;
+    }
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        if(isvalid(original,m,n)){
+            int temp = 0;
+            vector<vector<int>> v(m,vector<int> (n));
+            for(int i=0;i<original.size();i++){
+                v[i/n][i%n] = original[i];
+            }
+            return v;
+        }
+        return {};
+    }
+};
