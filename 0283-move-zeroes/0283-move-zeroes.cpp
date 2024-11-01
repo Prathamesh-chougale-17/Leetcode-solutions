430722e356406ea235ed04f4c1cb4d93c5315af4
PRATHAMESH CHOUGALE <106848418+Prathamesh-chougale-17@users.noreply.github.com>
2024-11-01 19:24:38 +0530

    Time: 130 ms (5%), Space: 21.7 MB (89.53%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n = nums.size(),count{0};
-        for(auto it=nums.begin();it!=nums.end();it++){
-            if(*it==0) {
-                nums.erase(it);
-                it--;
-                count++;
+        int n = nums.size();
+        int j = n-1;
+        for(int i=0;i<n;i++){
+            int j=i;
+            while(j>0 && nums[j-1]==0){
+                swap(nums[j],nums[j-1]);
+                j--;
             }
-        }
-        for(int i=0;i<count;i++){
-            nums.push_back(0);
         }
     }
 };
