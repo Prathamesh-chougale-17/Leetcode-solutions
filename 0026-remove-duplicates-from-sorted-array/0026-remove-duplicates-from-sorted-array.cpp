08609e7a0ee6042430110bb9345f84dd0cfba4fe
PRATHAMESH CHOUGALE <106848418+Prathamesh-chougale-17@users.noreply.github.com>
2024-11-01 13:55:08 +0530

    Time: 4 ms (12.81%), Space: 21.3 MB (57.06%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,15 +1,8 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        set<int> s;
-        for(auto &i:nums){
-            s.insert(i);
-        }
-        int j=0;
-        for(auto &i:s){
-            nums[j]=i;
-            j++;
-        }
-        return s.size();
+        auto it = unique(nums.begin(),nums.end());
+        nums.resize(distance(nums.begin(),it));
+        return nums.size();
     }
 };
