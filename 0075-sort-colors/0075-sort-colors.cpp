deba415d9a57bc8769afc09b3087db5a02e7d991
PRATHAMESH CHOUGALE <106848418+Prathamesh-chougale-17@users.noreply.github.com>
2024-11-02 22:47:30 +0530

    Time: 0 ms (100%), Space: 10.4 MB (35.1%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        int j=0,k=0,n=nums.size();
+        for(int i=0;i<n;i++){
+            int temp = nums[i];
+            // cout<<j<<k<<endl;
+            if(j<i && nums[j]>nums[i])
+                swap(nums[i],nums[j]);
+            if(k!=j && k<i)
+                swap(nums[k],nums[i]);
+            if(temp<1) j++;
+            if(temp<2) k++;
+        }
     }
 };
