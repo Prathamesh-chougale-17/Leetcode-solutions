0212fb6668ddad1a8364d23f37233c10af9e309e
PRATHAMESH CHOUGALE <106848418+Prathamesh-chougale-17@users.noreply.github.com>
2024-11-02 19:13:56 +0530

    Time: 3 ms (79.37%), Space: 15.4 MB (10.18%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for(int i=0;i<nums.size()-1;i++) {
-            for(int j=i+1;j<nums.size();j++){
-                if(nums[i]+nums[j]==target) return{i,j};
-            }
-        }
-        return {-1,-1};
+       unordered_map<int,int> um;
+       int n = nums.size();
+       for(int i=0;i<n;i++){
+            if(um[target-nums[i]]==0 || target-nums[i]!=nums[i])
+                um[target-nums[i]]=i+1;
+            if(um[nums[i]]>0 && (um[nums[i]]-1)!=i) return {um[nums[i]]-1,i};
+       }
+       return {-1};
     }
 };
