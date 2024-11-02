d61fc92da81391c15061d48d4cb9a930cd3e773c
PRATHAMESH CHOUGALE <106848418+Prathamesh-chougale-17@users.noreply.github.com>
2024-11-02 18:28:50 +0530

    Time: 0 ms (100%), Space: 19.6 MB (65.42%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i = 0; i < (nums.size()) / 2; i++){
-            if(nums[2 * i] != nums[2 * i + 1]) return nums[2 * i];
-        }
-        return nums[nums.size()-1];
+        int ans = 0;
+        for(auto &i:nums)
+            ans^=i;
+        return ans;
     }
 };
