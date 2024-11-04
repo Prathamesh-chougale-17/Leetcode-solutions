7acf47ed5eafa213d9445525d9d534892be424d4
PRATHAMESH CHOUGALE <106848418+Prathamesh-chougale-17@users.noreply.github.com>
2024-11-04 16:44:00 +0530

    Time: 1 ms (5.22%), Space: 14.6 MB (95.5%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n = nums.size(),isdec{1},breakpoint{0};
-        for(int i=n-2;i>=0;i--){
-            if(nums[i]<nums[i+1]){
-                breakpoint=i;
-                isdec=0;
+        int n = nums.size();
+        int bp = -1;
+        if(n<2){
+            return;
+        }
+        for (int i = n - 2; i >= 0; i--)
+        {
+            if (nums[i] < nums[i + 1])
+            {
+                bp = i;
                 break;
             }
         }
-        if(!isdec){
-            int jmax{INT_MAX},maxi{0},mini=nums[breakpoint],swapp{0};
-            for(int i=breakpoint+1;i<n;i++){
-                maxi = max(mini,nums[i]);
-                // jmax = min(maxi,jmax);
-                if(jmax>maxi && maxi!=mini){
-                    jmax = maxi;
-                    swapp = i;
-                    cout<<i<<maxi<<endl;
-                }
+        if (bp == -1)
+        {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+        for (int i = n - 1; i > bp; i--)
+        {
+            if (nums[i] > nums[bp])
+            {
+                swap(nums[i], nums[bp]);
+                break;
             }
-            swap(nums[breakpoint],nums[swapp]);
-            // cout<<*(nums.begin());
-            sort(nums.begin()+breakpoint+1,nums.end());
+            // cout<<nums[bp]<<" ";
         }
-        else{
-            sort(nums.begin(),nums.end());
-        }
+        reverse(nums.begin() + bp + 1, nums.end());
     }
 };
