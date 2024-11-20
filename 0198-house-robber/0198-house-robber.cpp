6e1b25dbabce621e4b57a4be40b2fa741ceb2d51
PRATHAMESH CHOUGALE <106848418+Prathamesh-chougale-17@users.noreply.github.com>
2024-11-20 19:03:29 +0530

    Time: 0 ms (100%), Space: 9.7 MB (12.8%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
-    int robHelper(vector<int> &nums, int n, vector<int> &dp) {
-        if(n<=0){
-            return 0;
-        }
-        if(dp[n-1]!=-1)return dp[n-1];
-        int take = nums[n-1] + robHelper(nums,n-2,dp);
-        int pass = robHelper(nums,n-1,dp);
-        return dp[n-1]=max(take,pass);
-    }
+    // int robHelper(vector<int> &nums, int n, vector<int> &dp) {
+    //     if(n<=0){
+    //         return 0;
+    //     }
+    //     if(dp[n-1]!=-1)return dp[n-1];
+    //     int take = nums[n-1] + robHelper(nums,n-2,dp);
+    //     int pass = robHelper(nums,n-1,dp);
+    //     return dp[n-1]=max(take,pass);
+    // }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n, -1); // Memoization array initialized to -1.
-        return robHelper(nums, n, dp);
+        vector<int> dp(n, 0);
+        // dp[0] = nums[0];
+        // dp[1] = max(nums[0], nums[1]);
+        int prev = nums[0];
+        if(n==0)return 0;
+        if(n==1)return prev;
+        int curr = max(nums[0],nums[1]);
+        // Fill the dp array iteratively
+        for (int i = 2; i < n; ++i) {
+            int temp = max(nums[i] + prev, curr);
+            prev = curr;
+            curr = temp;
+        }
+        return curr;
     }
 };
