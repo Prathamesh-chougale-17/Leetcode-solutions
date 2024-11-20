5901c4061828cc04d43f3b725f889d0680f1b443
PRATHAMESH CHOUGALE <106848418+Prathamesh-chougale-17@users.noreply.github.com>
2024-11-20 15:22:31 +0530

    Time: 0 ms (100%), Space: 9.7 MB (12.8%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
-    int robHelper(vector<int> &nums, int n, int curr, vector<int> &dp) {
-        if (curr >= n) return 0; // Base case: No more houses to rob.
-        if (dp[curr] != -1) return dp[curr]; // Use cached result.
-
-        // Two choices: Rob current house or skip it.
-        int rob = nums[curr] + robHelper(nums, n, curr + 2, dp); // Rob current, move to curr + 2.
-        int skip = robHelper(nums, n, curr + 1, dp); // Skip current, move to curr + 1.
-
-        dp[curr] = max(rob, skip); // Store and return the maximum profit.
-        return dp[curr];
+    int robHelper(vector<int> &nums, int n, vector<int> &dp) {
+        if(n<=0){
+            return 0;
+        }
+        if(dp[n-1]!=-1)return dp[n-1];
+        int take = nums[n-1] + robHelper(nums,n-2,dp);
+        int pass = robHelper(nums,n-1,dp);
+        return dp[n-1]=max(take,pass);
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n, -1); // Memoization array initialized to -1.
-        return robHelper(nums, n, 0, dp); // Start from index 0.
+        return robHelper(nums, n, dp);
     }
 };
