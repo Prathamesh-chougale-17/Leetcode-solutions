4c27148c0167986b66749bf5f6bb70a3f2a47756
PRATHAMESH CHOUGALE <106848418+Prathamesh-chougale-17@users.noreply.github.com>
2025-04-30 13:11:26 +0530

    Time: 3 ms (72.57%), Space: 14.9 MB (21.22%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,11 +4,10 @@ public:
         int n = nums.size();
         unordered_map<int,int> um;
         for(int i=0;i<n;i++){
-            if(um.find(nums[i])==um.end())
-                um[target-nums[i]] = i;
             if(um.find(nums[i])!=um.end() && (um[nums[i]]!=i)){
                 return {um[nums[i]],i};
             }
+            um[target-nums[i]] = i;
         }
         return {0};
     }
