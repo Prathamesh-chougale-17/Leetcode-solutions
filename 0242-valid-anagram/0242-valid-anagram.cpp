3cd4d5012593489daef43b255ca11d803667a5a2
PRATHAMESH CHOUGALE <106848418+Prathamesh-chougale-17@users.noreply.github.com>
2025-05-02 01:17:55 +0530

    Time: 7 ms (21.9%), Space: 9.9 MB (11.85%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,13 +3,13 @@ public:
     bool isAnagram(string s, string t) {
         int ss=s.size(),ts=t.size();
         if(ss!=ts)return false;
-        unordered_map<char,int> sm,tm;
+        unordered_map<char,int> sm;
         for(int i=0;i<ss;i++){
             sm[s[i]]+=1;
-            tm[t[i]]+=1;
+            sm[t[i]]-=1;
         }
         for(auto &i:s){
-            if(sm[i]!=tm[i])return false;
+            if(sm[i]!=0)return false;
         }
         return true;
     }
