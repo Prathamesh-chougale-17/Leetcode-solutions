c6c98f7ed8901598a606c9273f2154de4eb1b80b
PRATHAMESH CHOUGALE <106848418+Prathamesh-chougale-17@users.noreply.github.com>
2025-05-16 13:30:26 +0530

    Time: 11 ms (95.26%), Space: 60.6 MB (51.72%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.c
+++ 0948-sort-an-array/0948-sort-an-array.c
@@ -1,38 +1,25 @@
-#include <stdlib.h>
-#include <limits.h>
-
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* sortArray(int* nums, int numsSize, int* returnSize) {
-    int mn = INT_MAX, mx = INT_MIN;
-
-    // Find min and max in array
-    for (int i = 0; i < numsSize; i++) {
-        if (nums[i] < mn) mn = nums[i];
-        if (nums[i] > mx) mx = nums[i];
+    int max = nums[0], min = nums[0];
+    for(int i = 1; i < numsSize; i++){
+        if(min > nums[i]) min = nums[i];
+        else if(max < nums[i]) max = nums[i];
     }
-
-    int range = mx - mn + 1;
-    int* freq = (int*)calloc(range, sizeof(int));  // initialize frequency array
-
-    // Count frequency
-    for (int i = 0; i < numsSize; i++) {
-        freq[nums[i] - mn]++;
+    int range = max - min;
+    int count[range+1];
+    int *ans = (int *)malloc(numsSize*sizeof(int));
+    memset(count, 0, sizeof(count));
+    for(int i = 0; i < numsSize; i++){
+        count[nums[i]-min]++;
     }
-
-    // Allocate result array
-    int* result = (int*)malloc(numsSize * sizeof(int));
-    int index = 0;
-
-    // Reconstruct the sorted array
-    for (int i = 0; i < range; i++) {
-        while (freq[i]-- > 0) {
-            result[index++] = i + mn;
-        }
+    for(int i = 1; i <= range; i++){
+        count[i] += count[i-1];
     }
-
-    free(freq);  // cleanup
+    for(int i = numsSize-1; i >= 0; i--){
+        ans[count[nums[i]-min]---1] = nums[i];
+    }
     *returnSize = numsSize;
-    return result;
+    return ans;
 }
