f95b481724824da554d68dd0de719bb8032b6753
PRATHAMESH CHOUGALE <106848418+Prathamesh-chougale-17@users.noreply.github.com>
2025-10-22 10:38:12 +0530

    Time: 0 ms (100%), Space: 8.7 MB (44.19%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-       double ans = 1.0;
-       long long nn = n;
-       if(nn<0) nn = nn * -1;
-       while(nn){
-        if(nn%2){
-            ans = ans * x;
-            nn--;
-        }
+        if(n==0)return 1;
+        else if(n==1)return x;
+        else if(n==-1)return 1/x;
         else {
-            x = x * x;
-            nn /=2;
+            double z = myPow(x,n/2);
+            return z*z*myPow(x,n%2);
         }
-       } 
-       if(n<0) ans = (double)(1.0)/(double)(ans);
-       return ans;
     }
 };
