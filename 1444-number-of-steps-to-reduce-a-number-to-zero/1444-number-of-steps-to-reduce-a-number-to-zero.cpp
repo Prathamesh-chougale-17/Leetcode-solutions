690433c174982963a587cd949d0ca3cae2f08e1f
PRATHAMESH CHOUGALE <106848418+Prathamesh-chougale-17@users.noreply.github.com>
2025-11-01 21:58:03 +0530

    Time: 0 ms (100%), Space: 7.8 MB (98.4%) - LeetHub

--- 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.cpp
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int numberOfSteps(int num) {
         if(num==0) return 0;
-        if(num%2) return 1+numberOfSteps(num-1);
-        return 1+numberOfSteps(num/2);
+        return 1 + ((num%2==1)?numberOfSteps(num-1):numberOfSteps(num/2));
     }
 };
