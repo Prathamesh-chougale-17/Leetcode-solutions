d7bfd1afd579053b1aec084c6d7e9b0c7e935668
PRATHAMESH CHOUGALE <106848418+Prathamesh-chougale-17@users.noreply.github.com>
2026-06-06 07:45:05 +0530

    Time: 33 ms (11.79%), Space: 38.7 MB (20.13%) - LeetHub

---
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -0,0 +1,18 @@
+using pi = pair<int,int>;
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        priority_queue<pi> pq;
+        for(auto &i:arr){
+            pq.push({abs(i-x),i});
+            if(pq.size()>k)pq.pop();
+        }
+        vector<int> ans;
+        while(!pq.empty()){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
