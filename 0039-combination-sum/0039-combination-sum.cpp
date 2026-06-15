8f6c2e46c6a303f78798af410533316c392f1263
PRATHAMESH CHOUGALE <106848418+Prathamesh-chougale-17@users.noreply.github.com>
2026-06-15 16:19:42 +0530

    Time: 0 ms (100%), Space: 14 MB (74.35%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,22 +1,23 @@
+using vvi = vector<vector<int>>;
+using vi = vector<int>;
 class Solution {
 public:
-    void back(vector<vector<int>> &sol,vector<int> &tmp,vector<int> &can,int j,int n,int t){
-        if(t==0){
-            sol.push_back(tmp);
-            return;
-        }
-        if(t<0)return;
-        for(int i=j;i<n;i++){
-            tmp.push_back(can[i]);
-            back(sol,tmp,can,i,n,t-can[i]);
-            tmp.pop_back();
+    void back(vi &c,int t,vvi &ans,vi &sol,int i,int n){
+        if(t==0)
+            ans.push_back(sol);
+        if(t<=0)return;
+        for(int j = i;j<n;j++){
+            sol.push_back(c[j]);
+            back(c,t-c[j],ans,sol,j,n);
+            sol.pop_back();
         }
     }
-    vector<vector<int>> combinationSum(vector<int>& can, int t) {
-        vector<vector<int>> sol;
-        vector<int> tmp;
-        int n = can.size();
-        back(sol,tmp,can,0,n,t);
-        return sol;
+    vvi combinationSum(vi& c, int t) {
+        vvi ans;
+        vi sol;
+        int n = c.size();
+        int i = 0;
+        back(c,t,ans,sol,i,n);
+        return ans;
     }
 };
