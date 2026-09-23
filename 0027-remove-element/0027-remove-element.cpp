48b1200b7703b85ff08800d7c80dcb8636a2d0ce
PRATHAMESH CHOUGALE <106848418+Prathamesh-chougale-17@users.noreply.github.com>
2026-09-23 15:09:34 +0530

    Time: 0 ms (100%), Space: 11.8 MB (12.23%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int n = nums.size(),l = n - 1;
-        for(int i = l;i>=0;i--){
-            if(nums[i]==val){
-                cout<<i<<endl;
-                swap(nums[i],nums[l]);
-                l--;
-            }
+        int n = nums.size();
+        if(n==0)return 0;
+        int x = n-1;
+        while(nums[x]==val){
+            x--;
+            if(x<0)return 0;
         }
-        return l+1;        
+        for(int i=0;i<n;i++){
+            if(nums[i]==val){
+                swap(nums[i],nums[x]);
+                while(nums[x]==val)x--;
+            }
+            if(i==x)return x+1;
+        }
+        return x+1;
     }
 };
