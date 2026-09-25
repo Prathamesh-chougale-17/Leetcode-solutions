7768632fd0da19ea16ef6573d7de88016e5e9088
PRATHAMESH CHOUGALE <106848418+Prathamesh-chougale-17@users.noreply.github.com>
2026-09-25 15:49:16 +0530

    Time: 0 ms (100%), Space: 7.9 MB (73%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,10 +1,10 @@
-    vector<int> dp(46,0);
+vector<int> dp(46,-1);
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n<2)return 1;
-        if(dp[n]!=0)return dp[n];
-        dp[n] = climbStairs(n-1)+climbStairs(n-2);
-        return dp[n];
+        if(n<0)return 0;
+        if(n==0)return 1;
+        if(dp[n]!=-1)return dp[n];
+        return dp[n] = climbStairs(n-1) + climbStairs(n-2);
     }
 };
